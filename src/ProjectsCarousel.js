0699ebf93ff82e58040c2800cda7b0a96c2cd774
Andrew Cornell <andrewjc88@gmail.com>
2018-01-30 00:20:49 -0800

    Add link to live projects in carousel

--- src/ProjectsCarousel.js
+++ src/ProjectsCarousel.js
@@ -38,6 +38,7 @@ class ProjectsCarousel extends React.Component {
             <img className='SliderImg' src={images["MyReads.jpg"]} alt="My Reads app"/>
           </a>
           <p className="SliderText">Built a responsive web application that allows users to select and categorize books into a virtual bookshelf. Leveraged React to support a dynamic user interface that interacts with an API server and client library.</p>
+          <a className='Button' href="https://andrewjc88.github.io/myreads-app/">Project</a>
           <a className='Button' href="https://github.com/andrewjc88/myreads-app">github</a>
         </div>
         <div className='Slider'>
@@ -45,6 +46,7 @@ class ProjectsCarousel extends React.Component {
             <img className='SliderImg' src={images["DealQuest.jpg"]} alt="DealQuest app"/>
           </a>
           <p className="SliderText">A single-page web application, built using the Knockout.js, Zurb Foundation, Google maps API and groupon API, that displays a Map of an area and various points of interest. Users can search all Groupon deals by city or zip coade.</p>
+          <a className='Button' href="https://andrewjc88.github.io/DEAL_QUEST/">Project</a>
           <a className='Button' href="https://github.com/andrewjc88/DEAL_QUEST/">github</a>
         </div>
         <div className='Slider'>
@@ -52,13 +54,16 @@ class ProjectsCarousel extends React.Component {
             <img className='SliderImg' src={images["ClassicArcade.jpg"]} alt="Classic Aracde game"/>
           </a>
           <p className="SliderText">An HTML5 Canvas powered video game, developed using the best practices in Object Oriented JavaScript.</p>
+          <a className='Button' href="https://andrewjc88.github.io/classicArcadeGame/">Project</a>
           <a className='Button' href="https://github.com/andrewjc88/classicArcadeGame/">github</a>
         </div>
+
         <div className='Slider'>
           <a href="https://andrewjc88.github.io/frontend-nanodegree-feedreader/">
             <img className='SliderImg' src={images["FeedReader.jpg"]} alt="FeedReader app"/>
           </a>
           <p className="SliderText">Wrote comprehensive unit tests, using the Jasmine testing framework, for an RSS Feed Reader application that uses Google's RSS API.</p>
+          <a className='Button' href="https://andrewjc88.github.io/frontend-nanodegree-feedreader/">Project</a>
           <a className='Button' href="https://github.com/andrewjc88/frontend-nanodegree-feedreader/">github</a>
         </div>
       </Slider>
